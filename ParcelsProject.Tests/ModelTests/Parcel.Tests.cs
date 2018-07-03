8ce3e515e0cc092f2a0975f8ee365b2f7b5927c6
Abel Trotter and Devin Mounts <abel@>
2018-07-03 09:20:01 -0700

    Add remaining getters setters and tests, add CalcTotalVolume and test

--- ParcelsProject.Tests/ModelTests/Parcel.Tests.cs
+++ ParcelsProject.Tests/ModelTests/Parcel.Tests.cs
@@ -15,5 +15,56 @@ namespace ParcelsProject.Tests
             package.SetSender("Frodo");
             Assert.AreEqual("Frodo", package.GetSender());
         }
+
+        [TestMethod]
+        public void SetGetLength_SetsGetsLength_Int()
+        {
+            Parcel package = new Parcel();
+            package.SetLength(50);
+            Assert.AreEqual(50, package.GetLength());
+        }
+
+        [TestMethod]
+        public void SetGetWidth_SetsGetsWidth_Int()
+        {
+            Parcel package = new Parcel();
+            package.SetWidth(25);
+            Assert.AreEqual(25, package.GetWidth());
+        }
+
+        [TestMethod]
+        public void SetGetHeight_SetsGetsHeight_Int()
+        {
+            Parcel package = new Parcel();
+            package.SetHeight(10);
+            Assert.AreEqual(10, package.GetHeight());
+        }
+
+        [TestMethod]
+        public void SetGetWeight_SetsGetsWeight_Int()
+        {
+            Parcel package = new Parcel();
+            package.SetWeight(1000);
+            Assert.AreEqual(1000, package.GetWeight());
+        }
+
+        [TestMethod]
+        public void SetGetTotalVolume_SetsGetsTotalVolume_Int()
+        {
+            Parcel package = new Parcel();
+            package.SetTotalVolume(1000);
+            Assert.AreEqual(1000, package.GetTotalVolume());
+        }
+
+        [TestMethod]
+        public void CalculateVolume_CalculatesVolume_Int()
+        {
+            Parcel package = new Parcel();
+            package.SetHeight(20);
+            package.SetWidth(50);
+            package.SetLength(10);
+            package.CalculateVolume();
+            Assert.AreEqual(10000, package.GetTotalVolume());
+        }
     }
 }
--- ParcelsProject/Models/Parcel.cs
+++ ParcelsProject/Models/Parcel.cs
@@ -22,5 +22,73 @@ namespace ParcelsProject.Models
         {
             return _sender;
         }
+
+        public void SetLength(int length)
+        {
+            _length = length;
+        }
+
+        public int GetLength()
+        {
+            return _length;
+        }
+
+        public void SetWidth(int width)
+        {
+            _width = width;
+        }
+
+        public int GetWidth()
+        {
+            return _width;
+        }
+
+        public void SetHeight(int height)
+        {
+            _height = height;
+        }
+
+        public int GetHeight()
+        {
+            return _height;
+        }
+
+        public void SetWeight(int weight)
+        {
+            _weight = weight;
+        }
+
+        public int GetWeight()
+        {
+            return _weight;
+        }
+
+        public void SetTotalVolume(int volume)
+        {
+            _totalVolume = volume;
+        }
+
+        public int GetTotalVolume()
+        {
+            return _totalVolume;
+        }
+
+        public void SetCost(int cost)
+        {
+            _cost = cost;
+        }
+
+        public int GetCost()
+        {
+            return _cost;
+        }
+
+        public void CalculateVolume()
+        {
+            int volume = this.GetLength() * this.GetWidth() * this.GetHeight();
+            this.SetTotalVolume(volume);
+        }
+
+
     }
 }
